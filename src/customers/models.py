ed0db69a242313dcbc9711a44d56d29f9822d863
Rowan-W-Fitch <rfitch@oxy.edu>
2020-05-21 16:28:27 -0700

    added cart model

--- src/customers/models.py
+++ src/customers/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from restaurant_admin.models import MenuItem
 # Create your models here.
+
+class Cart(models.Model):
+    is_paid = models.BooleanField(default = False)
+    menu_items = models.ManyToManyField(MenuItem)
--- src/customers/urls.py
+++ src/customers/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.menu),
-    path('view_item', views.view_item),
-    path('view_cart', views.view_cart),
-    path('payment', views.payment),
-    path('order_confirmation', views.order_confirmation),
-
+    path('view_item/<int:restaurant_id>/<int:menu_id>/<int:item_id>', views.view_item),
+    path('add_item/<int:restaurant_id>/<int:menu_id>/<int:item_id>/<int:cart_id>', views.add_item), #for adding item to cart
+    path('remove_item/<int:restaurant_id>/<int:menu_id>/<int:item_id>/<int:cart_id>', views.remove_item), #for removing item from cart
+    path('view_cart/<int:cart_id>', views.view_cart),
+    path('payment/<int:cart_id>', views.payment),
+    path('order_confirmation/<int:cart_id>', views.order_confirmation),
 ]
