6b4043ae5de2e7237facb233e94af3b5756f9b8c
Luis Costa <luiscosta@Luiss-MBP.fritz.box>
2020-05-27 02:34:47 +0200

    tipping integrated into cashier interface (attribute for tips is in Cart model).

--- src/cashier/views.py
+++ src/cashier/views.py
@@ -13,7 +13,10 @@ def cashPaymentView(request):
             order_code = cd['order_code']
             curr_cart = Cart.objects.filter(cash_code=order_code).first()
             item_counters = MenuItemCounter.objects.filter(cart = curr_cart).all()
-            context = {'cart':curr_cart,'item_counters':item_counters}
+            tip_amount = round(curr_cart.tip*curr_cart.total,2)
+            grand_total = tip_amount + curr_cart.total
+            print(curr_cart.tip)
+            context = {'cart':curr_cart,'item_counters':item_counters,'tip_amount':tip_amount,'grand_total':grand_total}
             return render(request,'review_order2.html',context)
         else:
             print("here")
--- src/customers/models.py
+++ src/customers/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from restaurant_admin.models import MenuItem
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 """this model is synonymous with an order"""
@@ -8,6 +8,8 @@ class Cart(models.Model):
     is_paid = models.BooleanField(default = False)
     cash_code = models.CharField(null = True, max_length = 255) #the code generated if customer wants to pay cash
     total = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0.0)])
+    tip = models.DecimalField(default = 0.0, decimal_places=2, max_digits=12, validators=[MinValueValidator(0.0),MaxValueValidator(1.0)])
+    #tip is a percentage from 0-100 which is why we have two decimal places but if tip is 15% we simply store as 0.15.
 
 
 """ this model acts as a way to keep track of how many of a MenuItem are in a cart
