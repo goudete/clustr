d79d1fb357f792023199c5d860825c4d6b1064d9
Rowan-W-Fitch <rfitch@oxy.edu>
2020-06-02 12:50:35 -0700

    heroku

--- src/qr/settings.py
+++ src/qr/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import environ
 import django_heroku
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #env variables init
@@ -101,7 +102,7 @@ DATABASES = {
           'PORT': '',
     }
 }
-
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
