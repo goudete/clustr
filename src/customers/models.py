e970e54a61fffc034a38f518b68fc0c496c04bcc
goudete <goudetenrique@gmail.com>
2020-06-03 20:50:25 -0500

    edit cart complete

--- src/customers/models.py
+++ src/customers/models.py
@@ -11,6 +11,7 @@ class Cart(models.Model):
     stripe_order_id = models.CharField(null = True, max_length = 255) # The Stripe PaymentIntent API generates an id to reference paymentintent
     created_at = models.DateTimeField(auto_now_add=True)
     tip = models.DecimalField(default = 0.0, decimal_places=2, max_digits=12, validators=[MinValueValidator(0.0)])
+    custom_tip = models.BooleanField(default = False)
     total_with_tip = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0.0)])
     email = models.EmailField(max_length=200)
 
@@ -22,7 +23,7 @@ class MenuItemCounter(models.Model):
     item = models.ForeignKey(MenuItem, on_delete = models.CASCADE)
     quantity = models.PositiveIntegerField(default = 1)
     cart = models.ForeignKey(Cart, on_delete = models.CASCADE)
-    custom_instructions = models.CharField(null = True, blank= True, max_length = 255)
+    custom_instructions = models.CharField(null = True, blank= True, max_length = 255, default=None)
     price = models.DecimalField(decimal_places = 2, max_digits = 12, validators=[MinValueValidator(0.0)])
 
 class Feedback(models.Model):
--- src/customers/urls.py
+++ src/customers/urls.py
@@ -10,11 +10,13 @@ urlpatterns = [
     path('add_item/<int:cart_id>/<int:restaurant_id>/<int:menu_id>/<int:item_id>', views.add_item), #for adding item to cart
     path('remove_item/<int:cart_id>/<int:restaurant_id>/<int:menu_id>/<int:item_id>', views.remove_item), #for removing item from cart
     path('view_cart/<int:cart_id>/<int:restaurant_id>/<int:menu_id>', views.view_cart),
-    path('decrease_quantity<int:cart_id>/<int:restaurant_id>/<int:menu_id>', views.decrease_quantity),
+    # path('decrease_quantity<int:cart_id>/<int:restaurant_id>/<int:menu_id>', views.decrease_quantity),
     path('change_instructions/<int:cart_id>/<int:restaurant_id>/<int:menu_id>', views.change_instructions),
     path('payment/<int:cart_id>/<int:restaurant_id>/<int:menu_id>', views.payment),
     path('order_confirmation/<int:cart_id>', views.order_confirmation),
     path('calculate_tip/<int:cart_id>/<int:restaurant_id>/<int:menu_id>/<int:tip>', views.calculate_tip),
     path('email_receipt/<int:cart_id>', views.email_receipt),
     path('feedback/<int:cart_id>', views.feedback),
+    path('ajax/ajax_increase_quantity', views.ajax_increase_quantity, name='increase_quantity'),
+    path('ajax/ajax_decrease_quantity', views.ajax_decrease_quantity, name='decrease_quantity'),
 ]
