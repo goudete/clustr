abb0801acf7b4560f8411b708a7f78cbb6205fae
goudete <goudetenrique@gmail.com>
2020-06-04 20:17:01 -0500

    tweaks

--- src/qr/settings.py
+++ src/qr/settings.py
@@ -91,7 +91,7 @@ django_heroku.settings(locals())
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+# 
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
