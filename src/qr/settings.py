1ddb79d958e2b279706d0f89c23c7c670385bd8e
goudete <goudetenrique@gmail.com>
2020-06-11 16:52:15 -0500

    plz

--- src/qr/settings.py
+++ src/qr/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
           'PORT': '',
     }
 }
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
