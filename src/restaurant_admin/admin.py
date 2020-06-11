ba4e90f0011c78885020a0190c02cbc0819e0a3f
Luis Costa <luiscosta@Luiss-MBP.fritz.box>
2020-06-11 23:12:50 +0200

    email set up

--- src/restaurant_admin/admin.py
+++ src/restaurant_admin/admin.py
@@ -4,4 +4,7 @@ from .models import Restaurant, Menu, MenuItem
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(Menu)
-admin.site.register(MenuItem)
+
+class MenuItemAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+admin.site.register(MenuItem, MenuItemAdmin)
