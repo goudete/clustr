ef6b3e71792923f6845f974b77873df2ce14c153
Rowan-W-Fitch <rfitch@oxy.edu>
2020-06-13 14:49:21 -0700

    phone number validation

--- src/customers/forms.py
+++ src/customers/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import MenuItemCounter, Cart, Feedback
 from crispy_forms.helper import FormHelper
+from phonenumber_field.formfields import PhoneNumberField
+import phonenumbers
+from django.utils.translation import gettext as _
 
 class CustomOrderForm(forms.ModelForm):
     class Meta:
@@ -15,6 +18,23 @@ class CustomTipForm(forms.Form):
 
 class EmailForm(forms.Form):
     email_input = forms.EmailField(required=False, max_length=200)
+
+
+class PhoneForm(forms.Form):
+    def validate_phone_number(self):
+        num = self.cleaned_data.get('phone')
+        if phonenumbers.is_valid_number(num):
+            return num
+        else:
+            raise forms.ValidationError(_("Invalid Phone Number"))
+
+    phone = PhoneNumberField(required = False)
+
+    def __init__(self, *args, **kwargs):
+        super(PhoneForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_show_labels = False
+
 
 class FeedbackForm(forms.ModelForm):
     class Meta:
