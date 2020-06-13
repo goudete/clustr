69197fda7b27edf901e74d06040bea16afb3327c
goudete <goudetenrique@gmail.com>
2020-06-13 16:22:19 -0500

    heroku is a bitch

--- src/qr/settings.py
+++ src/qr/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(env_file)
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
