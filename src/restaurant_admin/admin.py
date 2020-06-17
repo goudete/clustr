0a67b9af0585851c894357fbe8a43d83f8f5eb89
goudete <goudetenrique@gmail.com>
2020-06-17 14:38:33 -0500

    adding SelectItem to admin

--- src/restaurant_admin/admin.py
+++ src/restaurant_admin/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Restaurant, Menu, MenuItem
+from .models import Restaurant, Menu, MenuItem, SelectOption
 
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(Menu)
+admin.site.register(SelectOption)
 
 class MenuItemAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
