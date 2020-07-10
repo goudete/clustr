9e527b239260b88008ac28762cf6f2fa9f8b1862
goudete <goudetenrique@gmail.com>
2020-07-10 10:18:33 -0500

    stripe tweak

--- src/customers/views.py
+++ src/customers/views.py
@@ -497,7 +497,7 @@ def payment(request, cart_id, restaurant_id, menu_id):
               payment_method_types=['card'],
               amount=int((cart.total_with_tip*100)),
               currency='mxn',
-              on_behalf_of=curr_rest.stripe_account_id
+              stripe_account=curr_rest.stripe_account_id,
 
                # Verify your integration in this guide by including this parameter
               # metadata={'integration_check': 'accept_a_payment'},
