8879f02df0810805a1b79bba90b8743f15269b39
goudete <goudetenrique@gmail.com>
2020-09-01 23:08:16 -0700

    bug

--- src/customers/views.py
+++ src/customers/views.py
@@ -542,8 +542,7 @@ def pick_up_or_delivery(request, cart_id, restaurant_id, menu_id):
         curr_cart = Cart.objects.filter(id = cart_id).first()
         curr_rest = Restaurant.objects.filter(id = restaurant_id).first()
         curr_menu = Menu.objects.filter(id = menu_id).first()
-        pri = OrderTracker.objects.filter(cart = curr_cart).exists() == False
-        print('ORDERTRACKER:', pri)
+
         if OrderTracker.objects.filter(cart = curr_cart).exists() == False:
             tracker = OrderTracker(restaurant = curr_cart.restaurant, cart = curr_cart, is_complete = False)
             tracker.save()
